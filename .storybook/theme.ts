99ca2ea711385b5eef52f37b281bde67777bce7c
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-05-21 15:29:22 +0530

    Customize the storybook dashboard to show new branding

---
+++ .storybook/theme.ts
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+
+export default create({
+    base: 'dark',
+    brandTitle: 'Uixcel',
+    brandUrl: 'https://github.com/dhruvdhaduk-simform/uixcel/',
+    brandImage: '/uixcelLogo.png',
+    brandTarget: '_self',
+});
